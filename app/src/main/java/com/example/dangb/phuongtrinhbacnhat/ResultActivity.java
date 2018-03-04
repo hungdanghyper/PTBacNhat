1edd3c13366eecef830eb242f09a65e5c0f25aa6
hungdanghyper <hungdanghyper@gmail.com>
2018-03-04 20:05:24 +0700

    back

--- app/src/main/java/com/example/dangb/phuongtrinhbacnhat/ResultActivity.java
+++ app/src/main/java/com/example/dangb/phuongtrinhbacnhat/ResultActivity.java
@@ -3,10 +3,13 @@ package com.example.dangb.phuongtrinhbacnhat;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
     public TextView text;
+    private Button btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +18,12 @@ public class ResultActivity extends AppCompatActivity {
         String result = intent.getStringExtra("result");
         text = findViewById(R.id.textView2);
         text.setText(result);
+        btn2 = findViewById(R.id.btn2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
